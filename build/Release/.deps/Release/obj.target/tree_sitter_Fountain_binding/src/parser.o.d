e2bb26f8de3c2e5af43e63b17a28e72880b66a0f
Ben Sobel <7thof14217@gmail.com>
2019-02-03 22:04:34 -0800

    Remove unnecessary files

--- build/Release/.deps/Release/obj.target/tree_sitter_Fountain_binding/src/parser.o.d
+++
@@ -1,5 +0,0 @@
-cmd_Release/obj.target/tree_sitter_Fountain_binding/src/parser.o := cc '-DNODE_GYP_MODULE_NAME=tree_sitter_Fountain_binding' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/benjamin/.node-gyp/9.11.2/include/node -I/home/benjamin/.node-gyp/9.11.2/src -I/home/benjamin/.node-gyp/9.11.2/deps/openssl/config -I/home/benjamin/.node-gyp/9.11.2/deps/openssl/openssl/include -I/home/benjamin/.node-gyp/9.11.2/deps/uv/include -I/home/benjamin/.node-gyp/9.11.2/deps/zlib -I/home/benjamin/.node-gyp/9.11.2/deps/v8/include -I../node_modules/nan -I../src  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -std=c99 -MMD -MF ./Release/.deps/Release/obj.target/tree_sitter_Fountain_binding/src/parser.o.d.raw   -c -o Release/obj.target/tree_sitter_Fountain_binding/src/parser.o ../src/parser.c
-Release/obj.target/tree_sitter_Fountain_binding/src/parser.o: \
- ../src/parser.c ../src/tree_sitter/parser.h
-../src/parser.c:
-../src/tree_sitter/parser.h:
